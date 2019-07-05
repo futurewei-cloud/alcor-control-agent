dbacc8207dd5e961349b618280db5617b2d65e8b
Sze Kwok Li (Eric) <s00488538@china.huawei.com>
2019-07-05 16:01:50 -0700

    update network control agent with logging support, and added task numbers

--- src/netwagent.cpp
+++ src/netwagent.cpp
@@ -1,11 +1,20 @@
+// c includes
+#include "nca_log.h"
+#include "../../Transit/src/rpcgen/trn_rpc_protocol.h" // TODO: need to use a better path
+
+// cpp includes
 #include <iostream>
 #include "messageproducer.h"
+
+#define NCALOGNAME "NetworkControlAgent"
 
 using namespace std;
 using messagemanager::MessageProducer;
 
-int main() {
-    
+int main(int argc, char **argv)
+{
+    NCA_LOG_INIT(NCALOGNAME);
+
     //Preload network agent configuration 
     //TODO: load it from configuration file 
     string host_id = "00000000-0000-0000-0000-000000000000";
@@ -14,16 +23,21 @@ int main() {
     int partition_value = 0;
 
     //Check if OVS and/or OVS daemon exists; if not, launch the program
+    //tracked by issue#10, may not needed based on discussion with XiaoNing
     
-    //Check if transit program exists; if not, launch the program 
+    //Check if transit program exists on Physical NIC; if not, launch the program 
+    //P0, tracked by issue#11
 
     //Announce this host (agent) and register in every kafka cluster
+    //P0, tracked by issue#12
     
     //Launch background threads to monitor and to emit network health status 
     //	for customer VMs, containers, as well as
     //	infra host services including OVS, transit etc.   
+    //P1, tracked by issue#13
     
-    //Upload or refresh the networking spec of this host (including DPDK, SR-IOV, bandwitdh etc.)
+    //Upload or refresh the networking spec of this host (including DPDK, SR-IOV, bandwidth etc.)
+    //P1, tracked by issue#14
     MessageProducer host_spec_producer(broker_list, topic_host_spec, partition_value);
     cout << "broker list:" << host_spec_producer.getBrokers() << endl;
     cout << "topic:" << host_spec_producer.getTopicName() << endl;
@@ -35,9 +49,14 @@ int main() {
     cout << "Publish completed" << endl;
 
     //Listen to Kafka clusters for any network configuration operations
+    //P0, tracked by issue#15
     
     //Receive and deserialize any new configuration
+    //P0, tracked by issue#16
     
     //Depending on different operations, program XDP through corresponding RPC apis by transit daemon
+    //P0, tracked by issue#17
+
+    NCA_LOG_CLOSE();
 }
 
