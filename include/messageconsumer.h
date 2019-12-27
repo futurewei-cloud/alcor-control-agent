a68b7023a5792d0cba930a3eada9a338d16d0b09
Eric Li <51724377+er1cthe0ne@users.noreply.github.com>
2019-12-27 15:37:49 -0800

    added license header for all .h files

--- include/messageconsumer.h
+++ include/messageconsumer.h
@@ -1,52 +1,62 @@
+// Copyright 2020 The Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef KAFKA_CONSUMER_H
 #define KAFKA_CONSUMER_H
 
 #include "cppkafka/consumer.h"
 #include "cppkafka/configuration.h"
 
-using std::string;
-using cppkafka::Consumer;
 using cppkafka::Configuration;
+using cppkafka::Consumer;
+using std::string;
 
-namespace messagemanager{
-
+namespace messagemanager
+{
 class MessageConsumer {
+  private:
+  string brokers_list; //IP addresses of Kafka brokers, format: <Kafka_host_ip>:<port>, example:10.213.43.188:9092
 
-private:
+  string group_id; //Group id of the Kafka consumer
 
-    string brokers_list;        //IP addresses of Kafka brokers, format: <Kafka_host_ip>:<port>, example:10.213.43.188:9092
+  string topic_name; //A string representation of the topic to be consumed, for example: /hostid/00000000-0000-0000-0000-000000000000/netwconf/
 
-    string group_id;            //Group id of the Kafka consumer
+  Configuration config; //Configuration of the Kafka consumer
 
-    string topic_name;     	//A string representation of the topic to be consumed, for example: /hostid/00000000-0000-0000-0000-000000000000/netwconf/
+  Consumer *ptr_consumer; //A pointer to the Kafka consumer
 
-    Configuration config;       //Configuration of the Kafka consumer
+  public:
+  MessageConsumer(string brokers, string group_id);
 
-    Consumer* ptr_consumer;     //A pointer to the Kafka consumer
+  ~MessageConsumer();
 
-public:
+  string getBrokers() const;
 
-    MessageConsumer(string brokers, string group_id);
+  string getLastTopicName() const;
 
-    ~MessageConsumer();
+  string getGroupId() const;
 
-    string getBrokers() const;
+  void setGroupId(string group_id);
 
-    string getLastTopicName() const;
+  bool consumeDispatched(string topic);
 
-    string getGroupId() const;
+  private:
+  void setBrokers(string brokers);
 
-    void setGroupId(string group_id);
-
-    bool consumeDispatched(string topic);
-
-private:
-
-    void setBrokers(string brokers);
-
-    void setLastTopicName(string topic);
+  void setLastTopicName(string topic);
 };
 
-} // meesagemanager
+} // namespace messagemanager
 
 #endif
