2f2608c5f9956f874256b309c6109029b044e190
Eric Li <51724377+er1cthe0ne@users.noreply.github.com>
2020-02-05 11:49:03 -0800

    fix directory in the build script

--- build/build.sh
+++ build/build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Build alcor-control-agent project
+# Initialize the needed mizar submodule
+git submodule update --init --recursive
 
 # Create and Start the build contrainer
-docker build -t aca_build0 .
-docker create -v ~/dev:/mnt/host/code -it --privileged --cap-add=NET_ADMIN --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --name a1 aca_build0:latest /bin/bash
+docker build -f build/Dockerfile -t aca_build0 .
+docker create -v $(pwd):/mnt/host/code -it --privileged --cap-add=NET_ADMIN --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --name a1 aca_build0:latest /bin/bash
 docker start a1
 
 # Build mizar first
-docker exec -it a1 bash -c "cd /mnt/host/code/alcor-control-agent/mizar && make"
+docker exec a1 bash -c "cd /mnt/host/code/mizar && make"
 
 # Build alcor control agent
-docker exec -it a1 bash -c "cd /mnt/host/code/alcor-control-agent && cmake . && make"
+docker exec a1 bash -c "cd /mnt/host/code && cmake . && make"
