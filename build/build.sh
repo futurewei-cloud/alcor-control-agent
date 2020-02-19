c6f3ae64a4b9417a7eab5c1775519d9fd0953ca4
Eric Li <51724377+er1cthe0ne@users.noreply.github.com>
2020-02-19 15:18:58 -0800

    submodule preparation

--- build/build.sh
+++ build/build.sh
@@ -4,12 +4,12 @@
 git submodule update --init --recursive
 
 # Create and Start the build contrainer
-docker build -f build/Dockerfile -t aca_build0 .
+docker build -f AlcorControlAgent/build/Dockerfile -t aca_build0 .
 docker create -v $(pwd):/mnt/host/code -it --privileged --cap-add=NET_ADMIN --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --name a1 aca_build0:latest /bin/bash
 docker start a1
 
 # Build mizar first
-docker exec a1 bash -c "cd /mnt/host/code/mizar && make"
+docker exec a1 bash -c "cd /mnt/host/code/AlcorControlAgent/mizar && make"
 
 # Build alcor control agent
-docker exec a1 bash -c "cd /mnt/host/code && cmake . && make"
+docker exec a1 bash -c "cd /mnt/host/code/AlcorControlAgent && cmake . && make"
