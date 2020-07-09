cdde01aaf2cfad9a9ab3a612bcf4e7237e20d176
James Chung <66383484+cj-chung@users.noreply.github.com>
2020-07-09 10:38:34 -0700

    Functions for tracking and parsing OVS packet-in message, and sending packet-out message back to OVS. (#112)
    * new commit
    
    * dockerfile
    
    * add ovs-ofctl commands dump-flows, monitor, packet-in, packet-out
    
    * commit Dockerfile
    
    * separate aca_* and ovs_* functions into two cpp files
    
    * separate aca_* and ovs_* functions into two cpp files.
    
    * separate aca_* and ovs_* functions into two cpp files
    
    * code clean up
    
    * remove aca_tests_ovs_control.cpp
    
    * remove testing codes
    
    * add comments
    
    * remove ovs-ctl script
    
    * add vconn-provider.h
    
    * remove unneccessary header files
    
    * commit
    
    * commit
    
    * commit

---
+++ include/aca_ovs_control.h
@@ -0,0 +1,139 @@
+// Copyright 2019 The Alcor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef ACA_OVS_CONTROL_H
+#define ACA_OVS_CONTROL_H
+
+#define IPTOS_PREC_INTERNETCONTROL 0xc0
+#define DSCP_DEFAULT (IPTOS_PREC_INTERNETCONTROL >> 2)
+#define STDOUT_FILENO   1   /* Standard output.  */
+
+#include <openvswitch/ofp-errors.h>
+#include <openvswitch/ofp-packet.h>
+#include <string>
+
+// OVS monitor implementation class
+namespace aca_ovs_control
+{
+class ACA_OVS_Control {
+  public:
+  static ACA_OVS_Control &get_instance();
+
+  /* 
+   * main function to call other function through command line
+   * It accepts three options:
+   *   -c <commands>, available commands are dump-flows, monitor, packet_out
+   *   -t <target bridge>, eg. br-int, br-tun
+   *   -o <additional options>, depends on the command
+   */
+  int control();
+
+  /* 
+   * create a monitor channel connecting to ovs.
+   * Input: 
+   *    const char *bridge: bridge name
+   *    cnost char *opt: option for monitor
+   * Availabe options: 
+   *    <miss-len>: a number larger than 1. 
+   *    resume: continually intercept packet-in message
+   */
+  void monitor(const char *bridge, const char *opt);
+  
+  /*
+   * send a packet back to ovs.
+   * Input:
+   *    const char *bridge: bridge name
+   *    cnost char *opt: option for packet_out
+   * Availabe options:
+   *    in_port=<in_port>: (required) in_port number on ovs, or controller, local
+   *    packet=<hex string>: (required) the hex string representation of a packet 
+   *    actions=<actions>: actions can be normal, control, or resumbit(,2) to a table 
+   * example:
+   *    ACA_OVS_Control::get_instance().packet_out("br-tun", 
+   *                            "in_port=controller packet=<hex-string> actions=normal"
+   */
+  void packet_out(const char *bridge, const char *opt);
+
+  void dump_flows(const char *bridge, const char *opt); 
+  void parse_packet(void *packet);
+  void print_payload(const u_char *payload, int len);
+  void print_hex_ascii_line(const u_char *payload, int len, int offset);
+
+  /* ethernet headers are always exactly 14 bytes [1] */
+  #define SIZE_ETHERNET 14
+
+  /* IP header */
+  struct sniff_ip {
+          u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
+          u_char  ip_tos;                 /* type of service */
+          u_short ip_len;                 /* total length */
+          u_short ip_id;                  /* identification */
+          u_short ip_off;                 /* fragment offset field */
+          #define IP_RF 0x8000            /* reserved fragment flag */
+          #define IP_DF 0x4000            /* dont fragment flag */
+          #define IP_MF 0x2000            /* more fragments flag */
+          #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
+          u_char  ip_ttl;                 /* time to live */
+          u_char  ip_p;                   /* protocol */
+          u_short ip_sum;                 /* checksum */
+          struct in_addr ip_src, ip_dst;  /* source and dest address */
+  };
+  /* The second-half of the first byte in ip_header contains the IP header length (IHL). */
+  #define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
+  /* The IHL is number of 32-bit segments. Multiply by four to get a byte count for pointer arithmetic */
+  #define IP_V(ip)                (((ip)->ip_vhl) >> 4)
+
+  /* TCP header */
+  typedef u_int tcp_seq;
+
+  struct sniff_tcp {
+          u_short th_sport;               /* source port */
+          u_short th_dport;               /* destination port */
+          tcp_seq th_seq;                 /* sequence number */
+          tcp_seq th_ack;                 /* acknowledgement number */
+          u_char  th_offx2;               /* data offset, rsvd */
+  #define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
+          u_char  th_flags;
+          #define TH_FIN  0x01
+          #define TH_SYN  0x02
+          #define TH_RST  0x04
+          #define TH_PUSH 0x08
+          #define TH_ACK  0x10
+          #define TH_URG  0x20
+          #define TH_ECE  0x40
+          #define TH_CWR  0x80
+          #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+          u_short th_win;                 /* window */
+          u_short th_sum;                 /* checksum */
+          u_short th_urp;                 /* urgent pointer */
+  };
+
+  /* UDP protocol header. */
+  struct sniff_udp {
+          u_short uh_sport;               /* source port */
+          u_short uh_dport;               /* destination port */
+          u_short uh_ulen;                /* udp length */
+          u_short uh_sum;                 /* udp checksum */
+  };
+
+  // compiler will flag the error when below is called.
+  ACA_OVS_Control(ACA_OVS_Control const &) = delete;
+  void operator=(ACA_OVS_Control const &) = delete;
+
+  private:
+  ACA_OVS_Control(){};
+  ~ACA_OVS_Control(){};
+};
+} // namespace aca_ovs_control
+#endif // #ifndef ACA_OVS_CONTROL_H
---
+++ src/ovs/aca_ovs_control.cpp
@@ -0,0 +1,312 @@
+// Copyright 2019 The Alcor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "aca_ovs_control.h"
+#include "ovs_control.h"
+#include "aca_log.h"
+#include "aca_util.h"
+#include <iostream>
+#include <vector> 
+#include <unordered_map> 
+#include <signal.h>
+#include <syslog.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <chrono>
+#include <errno.h>
+#include <stdexcept>
+#include <inttypes.h>
+#include <net/ethernet.h>
+#include <netinet/ether.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+
+using namespace std;
+using namespace ovs_control;
+
+extern string g_ofctl_command;
+extern string g_ofctl_target;
+extern string g_ofctl_options;
+
+namespace aca_ovs_control
+{
+ACA_OVS_Control &ACA_OVS_Control::get_instance()
+{
+  // Instance is destroyed when program exits.
+  // It is instantiated on first use.
+  static ACA_OVS_Control instance;  
+
+  return instance;
+}
+
+int ACA_OVS_Control::control()
+{
+    int overall_rc = EXIT_SUCCESS;
+
+    char target[g_ofctl_target.size() + 1];
+    g_ofctl_target.copy(target, g_ofctl_target.size() + 1);
+    target[g_ofctl_target.size()] = '\0';
+
+    char options[g_ofctl_options.size() + 1];
+    g_ofctl_options.copy(options, g_ofctl_options.size() + 1);
+    options[g_ofctl_options.size()] = '\0';
+
+    if (g_ofctl_command.compare("monitor") == 0) {
+        monitor(target, options);
+    } else if (g_ofctl_command.compare("dump-flows") == 0) {
+        dump_flows(target, options);
+    } else if (g_ofctl_command.compare("packet-out") == 0) {
+        packet_out(target, options);
+    } else {
+        cout << "Usage: -c <command> -t <target> -o <options>" << endl;
+        cout << "   commands: monitor, dump-flows, packet-out..." << endl;
+        cout << "   target: swtich name, such as br-int, br-tun, ..." << endl;
+        cout << "   options: " << endl;
+        cout << "      moinor: \"[miss-len] [invalid-ttl] [resume] [watch:format]\"" << endl;
+        cout << "      packet-out: \"in_port=<in_port> packet=<hex string> [actions=<actions>]\"" << endl;
+    }
+
+    return overall_rc;
+}
+
+void ACA_OVS_Control::dump_flows(const char *bridge, const char *opt) 
+{
+    OVS_Control::get_instance().dump_flows(bridge, opt);
+}
+
+void ACA_OVS_Control::monitor(const char *bridge, const char *opt) 
+{
+    OVS_Control::get_instance().monitor(bridge, opt);
+}
+
+void ACA_OVS_Control::packet_out(const char *bridge, const char *opt) 
+{
+    OVS_Control::get_instance().packet_out(bridge, opt);
+}
+
+void ACA_OVS_Control::parse_packet(void *packet)
+{
+    const struct ether_header *eth_header;
+    /* The packet is larger than the ether_header struct,
+    but we just want to look at the first part of the packet
+    that contains the header. We force the compiler
+    to treat the pointer to the packet as just a pointer
+    to the ether_header. The data payload of the packet comes
+    after the headers. Different packet types have different header
+    lengths though, but the ethernet header is always the same (14 bytes) */
+    eth_header = (struct ether_header *) packet;
+    
+    ACA_LOG_INFO("Source Mac: %s\n", ether_ntoa((ether_addr *) &eth_header->ether_shost));
+
+    int vlan_len = 0;
+    char *base = (char *) packet;
+    uint16_t ether_type = ntohs(*(uint16_t *) (base + 12));
+    if (ether_type == ETHERTYPE_VLAN) {
+        ACA_LOG_INFO("Ethernet Type: 802.1Q VLAN tagging (0x8100) \n");
+        ether_type = ntohs(*(uint16_t *) (base + 16));
+        vlan_len = 4;
+    } 
+    
+    if (ether_type == ETHERTYPE_ARP) {
+        ACA_LOG_INFO("Ethernet Type: ARP (0x0806) \n");
+        ACA_LOG_INFO("   From: %s\n", inet_ntoa(*(in_addr *) (base + 14 + vlan_len + 14)));
+        ACA_LOG_INFO("     to: %s\n", inet_ntoa(*(in_addr *) (base + 14 + vlan_len + 14 + 10)));
+    } else if (ether_type == ETHERTYPE_IP) {
+        ACA_LOG_INFO("Ethernet Type: IP (0x0800) \n");
+    } else if (ether_type == ETHERTYPE_REVARP) {
+        ACA_LOG_INFO("Ethernet Type: REVARP (0x8035) \n");
+    } else {
+        ACA_LOG_INFO("Ethernet Type: Cannot Tell!\n");
+        return;
+    }
+    
+    /* define/compute ip header offset */
+    const struct sniff_ip *ip = (struct sniff_ip*)(base + SIZE_ETHERNET + vlan_len);
+    int size_ip = IP_HL(ip)*4;
+    
+    if (size_ip < 20) {
+        // printf("   *** Invalid IP header length: %u bytes\n", size_ip);
+        return;
+    } else {
+        /* print source and destination IP addresses */
+        ACA_LOG_INFO("       From: %s\n", inet_ntoa(ip->ip_src));
+        ACA_LOG_INFO("         To: %s\n", inet_ntoa(ip->ip_dst));
+        
+        /* determine protocol */ 
+        switch(ip->ip_p) {
+            case IPPROTO_TCP:
+                ACA_LOG_INFO("   Protocol: TCP\n");
+                break;
+            case IPPROTO_UDP:
+                ACA_LOG_INFO("   Protocol: UDP\n");
+                break;
+            case IPPROTO_ICMP:
+                ACA_LOG_INFO("   Protocol: ICMP\n");
+                break;
+            case IPPROTO_IP:
+                ACA_LOG_INFO("   Protocol: IP\n");
+                break;
+            default:
+                ACA_LOG_INFO("   Protocol: unknown\n");
+        }
+    }
+    /* define/compute tcp header offset */
+    const struct sniff_tcp *tcp = (struct sniff_tcp*)(base + SIZE_ETHERNET + vlan_len + size_ip);
+    const unsigned char *payload; 
+    int size_payload;
+    int size_tcp = TH_OFF(tcp)*4;
+
+    if (size_tcp < 20) {
+        // printf("   *** Invalid TCP header length: %u bytes\n", size_tcp);
+        return;
+    } else {    
+        ACA_LOG_INFO("   Src port: %d\n", ntohs(tcp->th_sport));
+        ACA_LOG_INFO("   Dst port: %d\n", ntohs(tcp->th_dport));
+        
+        /* define/compute tcp payload (segment) offset */
+        payload = (u_char *)(base + SIZE_ETHERNET + vlan_len + size_ip + size_tcp);
+        
+        /* compute tcp payload (segment) size */
+        size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
+        
+        /*
+        * Print payload data;
+        */
+        if (size_payload > 0) {
+            ACA_LOG_INFO("   Payload (%d bytes):\n", size_payload);
+            print_payload(payload, size_payload);
+        }
+    }
+
+     /* define/compute udp header offset */
+    const struct sniff_udp *udp = (struct sniff_udp*)(base + SIZE_ETHERNET + vlan_len + size_ip);
+    int size_udp = ntohs(udp->uh_ulen);
+    
+    if (size_udp < 20) {
+        return;
+    } else {
+        int udp_sport = ntohs(udp->uh_sport);
+        int udp_dport = ntohs(udp->uh_dport);
+        ACA_LOG_INFO("   Src port: %d\n", udp_sport);
+        ACA_LOG_INFO("   Dst port: %d\n", udp_dport);
+        
+        if (udp_sport == 68 && udp_dport == 67) {
+            ACA_LOG_INFO("   Message Type: DHCP\n");
+        }
+
+        /* define/compute udp payload (daragram) offset */
+        payload = (u_char *)(base + SIZE_ETHERNET + vlan_len + size_ip + 8);
+
+        /* compute udp payload (datagram) size */
+        size_payload = ntohs(ip->ip_len) - (size_ip + 8);
+
+        /*
+        * Print payload data.
+        */
+        if (size_payload > 0) {
+            ACA_LOG_INFO("   Payload (%d bytes):\n", size_payload);
+            print_payload(payload, size_payload);
+        }
+    }
+}
+
+/*
+ * print packet payload data (avoid printing binary data)
+ */
+void
+ACA_OVS_Control::print_payload(const u_char *payload, int len)
+{
+
+    int len_rem = len;
+    int line_width = 16;   /* number of bytes per line */
+    int line_len;
+    int offset = 0;     /* zero-based offset counter */
+    const u_char *ch = payload;
+
+    if (len <= 0)
+    return;
+
+    /* data fits on one line */
+    if (len <= line_width) {
+        print_hex_ascii_line(ch, len, offset);
+    } else {
+        /* data spans multiple lines */
+        for ( ;; ) {
+            /* compute current line length */
+            line_len = line_width % len_rem;
+            /* print line */
+            print_hex_ascii_line(ch, line_len, offset);
+            /* compute total remaining */
+            len_rem = len_rem - line_len;
+            /* shift pointer to remaining bytes to print */
+            ch = ch + line_len;
+            /* add offset */
+            offset = offset + line_width;
+            /* check if we have line width chars or less */
+            if (len_rem <= line_width) {
+                /* print last line and get out */
+                print_hex_ascii_line(ch, len_rem, offset);
+                break;
+            }
+        }
+    }
+}
+
+/*
+ * print data in rows of 16 bytes: offset   hex   ascii
+ * 00000   47 45 54 20 2f 20 48 54  54 50 2f 31 2e 31 0d 0a   GET / HTTP/1.1..
+ */
+void
+ACA_OVS_Control::print_hex_ascii_line(const u_char *payload, int len, int offset)
+{
+    int i;
+    int gap;
+    const u_char *ch;
+
+    /* offset */
+    ACA_LOG_INFO("%05d   ", offset);
+    
+    /* hex */
+    ch = payload;
+    for(i = 0; i < len; i++) {
+        ACA_LOG_INFO("%02x ", *ch);
+        ch++;
+        /* print extra space after 8th byte for visual aid */
+        if (i == 7) ACA_LOG_INFO(" ");
+    }
+    /* print space to handle line less than 8 bytes */
+    if (len < 8) ACA_LOG_INFO(" ");
+    
+    /* fill hex gap with spaces if not full line */
+    if (len < 16) {
+        gap = 16 - len;
+        for (i = 0; i < gap; i++) {
+            ACA_LOG_INFO("   ");
+        }
+    }
+    ACA_LOG_INFO("   ");
+    
+    /* ascii (if printable) */
+    ch = payload;
+    for(i = 0; i < len; i++) {
+        if (isprint(*ch)) ACA_LOG_INFO("%c", *ch);
+        else ACA_LOG_INFO(".");
+        ch++;
+    }
+    ACA_LOG_INFO("\n");
+    return;
+}
+
+} // namespace aca_ovs_control
