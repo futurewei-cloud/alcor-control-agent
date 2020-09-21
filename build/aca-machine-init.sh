36e345aa71f9e1487b00c03d3e6498d7fc53c822
lly <lly00@mail.ustc.edu.cn>
2020-09-22 01:48:20 +0800

    mkdir for apache-pulsar-client

--- build/aca-machine-init.sh
+++ build/aca-machine-init.sh
@@ -118,7 +118,7 @@ echo "6--- installing openvswitch dependancies ---" && \
 
 PULSAR_RELEASE_TAG='pulsar-2.6.1'
 echo "7--- installing pulsar dependacies ---" && \
-    mkdir /var/local/git/pulsar
+    mkdir /var/local/git/pulsar && \
     wget "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=pulsar/"${PULSAR_RELEASE_TAG}"/DEB/apache-pulsar-client.deb" -O /var/local/git/pulsar/apache-pulsar-client.deb && \
     wget "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=pulsar/"${PULSAR_RELEASE_TAG}"/DEB/apache-pulsar-client-dev.deb" -O /var/local/git/pulsar/apache-pulsar-client-dev.deb && \
     cd /var/local/git/pulsar && \
