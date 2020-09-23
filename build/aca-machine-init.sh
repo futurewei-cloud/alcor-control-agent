d9c51a42e76a88e25bc79c44af24e3529b42a5b9
lly <lly00@mail.ustc.edu.cn>
2020-09-23 14:20:00 +0800

    fix building bug

--- build/aca-machine-init.sh
+++ build/aca-machine-init.sh
@@ -119,11 +119,11 @@ echo "6--- installing openvswitch dependancies ---" && \
 PULSAR_RELEASE_TAG='pulsar-2.6.1'
 echo "7--- installing pulsar dependacies ---" && \
     mkdir /var/local/git/pulsar && \
-    wget "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=pulsar/"${PULSAR_RELEASE_TAG}"/DEB/apache-pulsar-client.deb" -O /var/local/git/pulsar/apache-pulsar-client.deb && \
-    wget "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=pulsar/"${PULSAR_RELEASE_TAG}"/DEB/apache-pulsar-client-dev.deb" -O /var/local/git/pulsar/apache-pulsar-client-dev.deb && \
+    wget https://archive.apache.org/dist/pulsar/${PULSAR_RELEASE_TAG}/DEB/apache-pulsar-client.deb -O /var/local/git/pulsar/apache-pulsar-client.deb && \
+    wget https://archive.apache.org/dist/pulsar/${PULSAR_RELEASE_TAG}/DEB/apache-pulsar-client-dev.deb -O /var/local/git/pulsar/apache-pulsar-client-dev.deb && \
     cd /var/local/git/pulsar && \
-    apt install ./apache-pulsar-client*.deb && \
-    rm /var/local/git/pulsar && \
+    apt install -y ./apache-pulsar-client*.deb && \
+    rm -rf /var/local/git/pulsar 
     cd ~
 
 echo "8--- building alcor-control-agent"
