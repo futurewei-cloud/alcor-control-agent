9bb67d7b3a9a7c639feb08d571895d65812d9ae8
PikaPikaW <wwcemail@163.com>
2021-02-04 17:08:16 +0800

    [Setup Travis CI] some clean up and test

--- test/travis_test/build_2_container.sh
+++ test/travis_test/build_2_container.sh
@@ -10,36 +10,35 @@ echo "code path is $code_dir"
 # git submodule update --init --recursive
 
 # Create and Start the build contrainer
-echo "    --- images_1 ---"
+echo "    --- images list ---"
 docker images
+echo "    --- build image ---"
 docker build -f $code_dir/build/Dockerfile -t aca_build0 .
-echo "    --- images_2 ---"
+echo "    --- images list ---"
 docker images
-echo "    --- container-1 ---"
+echo "    --- container list ---"
 docker ps -a
+echo "    --- create container ---"
 docker rm -f aca_PARENT || true
 docker create -v $code_dir:/mnt/host/code -it --privileged --cap-add=NET_ADMIN --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --name aca_PARENT aca_build0:latest /bin/bash
 docker start aca_PARENT
-
 docker rm -f aca_CHILD || true
 docker create -v $code_dir:/mnt/host/code -it --privileged --cap-add=NET_ADMIN --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --name aca_CHILD aca_build0:latest /bin/bash
 docker start aca_CHILD
-echo "    --- container-2 ---"
+echo "    --- container list ---"
 docker ps -a
-# Build mizar first
-# docker exec a1 bash -c "cd /mnt/host/code/mizar && make"
 
 if [ "$1" == "2_port_test" ]; then
   echo "    --- building alcor-control-agent ---"
   docker exec aca_CHILD bash -c "cd /mnt/host/code && cmake . && make"
   docker exec aca_PARENT bash -c "cd /mnt/host/code && cmake . && make"
-  # echo "    --- running alcor-control-agent ---"
-  # sends output to null device, but stderr to console 
-  # docker exec aca_CHILD bash -c "nohup /mnt/host/code/build/bin/AlcorControlAgent -d > /dev/null 2>&1 &"
-  # docker exec aca_PARENT bash -c "nohup /mnt/host/code/build/bin/AlcorControlAgent -d > /dev/null 2>&1 &"
-  echo "    --- running 2_ports_CREATE_test ---"
+  echo "    --- running test case---"
+  echo "    --- basic test case---"
+  # run basic test case, ./build/tests/aca_tests
+  docker exec aca_CHILD bash -c "cd /mnt/host/code && ./build/tests/aca_tests"
+  echo "    --- 2_ports_CREATE test case---"
   # run DISABLED_2_ports_CREATE_test_traffic_PARENT and DISABLED_2_ports_CREATE_test_traffic_CHILD
-  docker exec aca_CHILD bash -c "cd /mnt/host/code && ./build/tests/aca_tests --gtest_also_run_disabled_tests --gtest_filter=DISABLED_2_ports_CREATE_test_traffic_CHILD"
-  docker exec aca_PARENT bash -c "cd /mnt/host/code && ./build/tests/aca_tests --gtest_also_run_disabled_tests --gtest_filter=DISABLED_2_ports_CREATE_test_traffic_PARENT"
+  docker exec aca_CHILD bash -c "cd /mnt/host/code && ./build/tests/aca_tests --gtest_also_run_disabled_tests --gtest_filter=*DISABLED_2_ports_CREATE_test_traffic_CHILD"
+  docker exec aca_PARENT bash -c "cd /mnt/host/code && ./build/tests/aca_tests --gtest_also_run_disabled_tests --gtest_filter=*DISABLED_2_ports_CREATE_test_traffic_PARENT"
   
 fi
