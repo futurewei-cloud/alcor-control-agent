a84ab46f9797a330b2c45de0bb8857c1a15b7ef8
Rio Zhu <zzhu@futurewei.com>
2021-03-02 20:05:20 +0000

    Tried to create docker network, assign IP addresses to containers, and then run the create traffic tests with child/parent IPs specified

--- test/travis_test/build_2_container.sh
+++ test/travis_test/build_2_container.sh
@@ -19,14 +19,16 @@ if [ "$1" == "compile_and_run_unit_test" ]; then
   docker images
   echo "    --- container list ---"
   docker ps -a
-  echo "    --- create container ---"
+  echo "    --- create network ---"
+  docker network create --subnet=172.18.0.0/16 mynet123
+  echo "    --- create container with IP addresses assigned ---"
   docker rm -f aca_PARENT || true
-  docker create -v $code_dir:/mnt/host/code -it --privileged --cap-add=NET_ADMIN --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --name aca_PARENT aca_build0:latest /bin/bash
+  docker create -v $code_dir:/mnt/host/code -it --privileged --ip 172.18.0.2 --cap-add=NET_ADMIN --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --name aca_PARENT aca_build0:latest /bin/bash
   docker start aca_PARENT
   docker exec aca_PARENT bash -c "/etc/init.d/openvswitch-switch restart && \
   ovs-vswitchd --pidfile --detach"
   docker rm -f aca_CHILD || true
-  docker create -v $code_dir:/mnt/host/code -it --privileged --cap-add=NET_ADMIN --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --name aca_CHILD aca_build0:latest /bin/bash
+  docker create -v $code_dir:/mnt/host/code -it --privileged  --ip 172.18.0.3 --cap-add=NET_ADMIN --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --name aca_CHILD aca_build0:latest /bin/bash
   docker start aca_CHILD
   docker exec aca_CHILD bash -c "/etc/init.d/openvswitch-switch restart && \
   ovs-vswitchd --pidfile --detach"
@@ -43,7 +45,7 @@ if [ "$1" == "compile_and_run_unit_test" ]; then
 elif [ "$1" == "2_port_test_traffic" ]; then
   echo "    --- 2_ports_CREATE test case---"
   # run DISABLED_2_ports_CREATE_test_traffic_PARENT and DISABLED_2_ports_CREATE_test_traffic_CHILD
-  docker exec aca_CHILD bash -c "cd /mnt/host/code && ./build/tests/aca_tests --gtest_also_run_disabled_tests --gtest_filter=*DISABLED_2_ports_CREATE_test_traffic_CHILD" &
+  docker exec aca_CHILD bash -c "cd /mnt/host/code && ./build/tests/aca_tests --gtest_also_run_disabled_tests --gtest_filter=*DISABLED_2_ports_CREATE_test_traffic_CHILD -p 172.18.0.2" &
   sleep 5
-  docker exec aca_PARENT bash -c "cd /mnt/host/code && ./build/tests/aca_tests --gtest_also_run_disabled_tests --gtest_filter=*DISABLED_2_ports_CREATE_test_traffic_PARENT"
+  docker exec aca_PARENT bash -c "cd /mnt/host/code && ./build/tests/aca_tests --gtest_also_run_disabled_tests --gtest_filter=*DISABLED_2_ports_CREATE_test_traffic_PARENT -c 172.18.0.3"
 fi
