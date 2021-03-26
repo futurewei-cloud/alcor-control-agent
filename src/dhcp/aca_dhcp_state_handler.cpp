62af3451fa5ebc69e39c509edef177dd448b0ab2
Rio Zhu <zzhu@futurewei.com>
2021-03-26 21:53:35 +0000

    added logs in port state checking

--- src/dhcp/aca_dhcp_state_handler.cpp
+++ src/dhcp/aca_dhcp_state_handler.cpp
@@ -78,8 +78,16 @@ int Aca_Dhcp_State_Handler::update_dhcp_state_workitem(const DHCPState current_D
   }
   alcor::schema::OperationType current_operation_type =
           current_DhcpState.operation_type();
+  ACA_LOG_INFO("%s, port_states size: %d", "Searching in port states\n",
+               parsed_struct.port_states().size());
+
   if (current_operation_type == alcor::schema::OperationType::UPDATE) {
     for (int i = 0; i < parsed_struct.port_states().size(); i++) {
+      ACA_LOG_INFO(
+              "Port %d MAC: %s, device_ID: %s, device_owner: %s", i,
+              (parsed_struct.port_states().at(i).configuration().mac_address()).c_str(),
+              (parsed_struct.port_states().at(i).configuration().device_id()).c_str(),
+              (parsed_struct.port_states().at(i).configuration().device_owner()).c_str());
       if (parsed_struct.port_states().at(i).configuration().mac_address() ==
                   current_DhcpConfiguration.mac_address() &&
           !parsed_struct.port_states().at(i).configuration().device_id().empty() &&
