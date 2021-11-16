22d15fe3ff1ecaccb96f70f07cce3ca629f8a3c6
Rio Zhu <zzhu@futurewei.com>
2021-11-16 19:12:49 +0000

    Try to not delete the call object

--- src/comm/aca_grpc.cpp
+++ src/comm/aca_grpc.cpp
@@ -221,7 +221,7 @@ void GoalStateProvisionerAsyncServer::ProcessPushGoalStatesStreamAsyncCall(
       ACA_LOG_DEBUG("%s\n", "Nothing to do when a PushGoalStatesStream call in at SENT state");
       streamingCall->stream_.Finish(Status::OK, baseCall);
       streamingCall->gsOperationReply_.Clear();
-      delete baseCall;
+//       delete baseCall;
       break;
     default:
       break;
