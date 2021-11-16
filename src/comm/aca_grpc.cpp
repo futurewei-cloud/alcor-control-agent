15080296aa8543f94520050a790a1de84ebd007e
Rio Zhu <zzhu@futurewei.com>
2021-11-16 01:29:39 +0000

    Tried to let client know a call is finished

--- src/comm/aca_grpc.cpp
+++ src/comm/aca_grpc.cpp
@@ -220,7 +220,7 @@ void GoalStateProvisionerAsyncServer::ProcessPushGoalStatesStreamAsyncCall(
       break;
     case AsyncGoalStateProvionerCallBase::CallStatus::SENT:
       ACA_LOG_DEBUG("%s\n", "Nothing to do when a PushGoalStatesStream call in at SENT state");
-      streamingCall->stream_.Finish(Status::OK, baseCall);
+      streamingCall->stream_.Finish(Status::OK, static_cast<PushGoalStatesStreamAsyncCall *>(baseCall));
       delete streamingCall;
       break;
     default:
