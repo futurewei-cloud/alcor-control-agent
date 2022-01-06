55347bc768a785d32a5b22b749f2b0f7693b5a01
Rio Zhu <zzhu@futurewei.com>
2022-01-06 19:51:06 +0000

    _serialize_arp_message isn't a bottleneck?

--- src/ovs/aca_arp_responder.cpp
+++ src/ovs/aca_arp_responder.cpp
@@ -249,6 +249,10 @@ void ACA_ARP_Responder::arp_xmit(uint32_t in_port, void *vlanmsg, void *message,
     //ACA_LOG_ERROR("%s", "Serialized ARP Reply is null!\n");
     return;
   }
+  packet_out_counter ++;
+  if (1){
+    return;
+  }
   if (is_found) {
     options = inport + whitespace + packetpre + packet + whitespace + action;
     //delete the constructed arp reply
@@ -440,10 +444,7 @@ string ACA_ARP_Responder::_serialize_arp_message(vlan_message *vlanmsg, arp_mess
   //arp protocol：0806
   packet_header.append("0806");
   packet.insert(0, packet_header);
-  packet_out_counter ++;
-  if (1){
-    return string();
-  }
+
   return packet;
 }
 } // namespace aca_arp_responder
