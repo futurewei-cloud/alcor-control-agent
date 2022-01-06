6e5b5964ada40a568b0e638703d83cb55d1de062
Rio Zhu <zzhu@futurewei.com>
2022-01-06 23:38:38 +0000

    place counter in ofcontroller::packet_out

--- src/ovs/aca_ovs_l2_programmer.cpp
+++ src/ovs/aca_ovs_l2_programmer.cpp
@@ -698,10 +698,6 @@ void ACA_OVS_L2_Programmer::packet_out(int of_connection_id, const char *options
   // auto openflow_client_start = chrono::steady_clock::now();
 
   if (NULL != ofctrl) {
-      packet_out_counter ++;
-      if (1){
-        return;
-      }
       ofctrl->packet_out(of_connection_id, options);
   } else {
       ACA_LOG_ERROR("%s", "ACA_OVS_L2_Programmer::packet_out didn't find OF controller\n");
