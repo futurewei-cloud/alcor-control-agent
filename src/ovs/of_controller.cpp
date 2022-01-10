423e74e36f18fc9bdfac36fa3719dd2a9b4597f8
Rio Zhu <zzhu@futurewei.com>
2022-01-10 21:25:25 +0000

    enabled send packet out again, and test if cbench's OFPT_VENDOR should also count

--- src/ovs/of_controller.cpp
+++ src/ovs/of_controller.cpp
@@ -75,9 +75,9 @@ void OFController::message_callback(OFConnection* ofconn, uint8_t type, void* da
         //        std::bind(&aca_on_demand_engine::ACA_On_Demand_Engine::parse_packet,
         //                  &aca_on_demand_engine::ACA_On_Demand_Engine::get_instance(),
         //                  in_port, (void *)pin->data(), ofconn->get_id()));
-        std::string flow_string = "table=1,nw_dst=127.0.0.1,priority=1,actions=drop";
-        execute_flow(ofconn->get_id(), flow_string);
-        /*
+        // std::string flow_string = "table=1,nw_dst=127.0.0.1,priority=1,actions=drop";
+        // execute_flow(ofconn->get_id(), flow_string);
+        
         marl::schedule([=] {
             fluid_msg::of10::PacketIn* pin = new fluid_msg::of10::PacketIn();
             pin->unpack((uint8_t *) data);
@@ -90,7 +90,7 @@ void OFController::message_callback(OFConnection* ofconn, uint8_t type, void* da
                     ofconn->get_id());
             // std::this_thread::sleep_for(std::chrono::microseconds(100));
         });
-        */
+        
     } else if (type == 33) { // OFPRAW_OFPT14_BUNDLE_CONTROL
         auto t = std::chrono::high_resolution_clock::now();
 
