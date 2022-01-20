c918bfd17c4761e687de5c17c74d52d574143db4
Rio Zhu <zzhu@futurewei.com>
2022-01-20 00:30:18 +0000

    revert back changes

--- src/ovs/aca_vlan_manager.cpp
+++ src/ovs/aca_vlan_manager.cpp
@@ -204,17 +204,11 @@ int ACA_Vlan_Manager::create_l2_neighbor(string virtual_ip, string virtual_mac,
                          "->NXM_NX_TUN_ID[],set_field:" + remote_host_ip +
                          "->tun_dst,output:" + VXLAN_GENERIC_OUTPORT_NUMBER;
 
-  // aca_ovs_l2_programmer::ACA_OVS_L2_Programmer::get_instance().execute_openflow(culminative_time,
-  //                                                                               "br-tun",
-  //                                                                               match_string + action_string,
-  //                                                                               "add");
-  auto culminative_time_ptr = & culminative_time;
-  marl::schedule([=]{
-    aca_ovs_l2_programmer::ACA_OVS_L2_Programmer::get_instance().execute_openflow(*culminative_time_ptr,
+  aca_ovs_l2_programmer::ACA_OVS_L2_Programmer::get_instance().execute_openflow(culminative_time,
                                                                                 "br-tun",
                                                                                 match_string + action_string,
                                                                                 "add");
-  });
+
   // create arp entry in arp responder for the l2 neighbor
   stArpCfg.mac_address = virtual_mac;
   stArpCfg.ipv4_address = virtual_ip;
